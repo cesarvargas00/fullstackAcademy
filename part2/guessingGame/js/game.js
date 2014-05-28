a028257f670fe7c22b04b9773b7190c0fad69838
Cesar Vargas <cesarvargas@Cesars-MacBook-Pro.local>
2014-05-28 03:31:02 -0400

    Oops, guess I was wrong... Still a few things to do with the game.js... Gonna go sleeping now ,cause I'm tired. Finish this l8r first thing in the morning.

--- part2/guessingGame/js/game.js
+++ part2/guessingGame/js/game.js
@@ -1,5 +1,104 @@
+/*
+Things to do:
+
+1 - Track the user's previous guess. Let them know if they are getting “hotter” or “colder” based on their previous guess.
+2 - Reset button does not remove disabled from other buttons, yet.
+3 - CSS this!
+*/
 
 $(document).ready(function(){
+
+
+	var theNumber = generateRandom(); //random number
+	var lifes = 5; //Amount of permitted guesses before GAME OVER
+	var guesses = []; // What the user tried so far.
+
+	function generateRandom () {
+		//this function generates a random number between 1 & 100.
+		return Math.round(Math.random() * 100 + 1);
+	}
+
+	function resetGame(){
+		//self explanatory
+		theNumber = generateRandom();
+		lifes = 5;
+		guesses	= [];
+	}
+
+	function play(number){
+		//main play function :D yayy
+		var intNumber = parseInt(number);
+		var feedback = "";
+		//if no lifes, GAME OVER, son.
+		if (lifes > 0) {
+			//do a small validation wheter it is a number.
+			if(isNaN(intNumber)){
+				$("#feedback").html("Must choose a number between 1 and 100!");
+				console.log("Must choose a number between 1 and 100!");
+				return 0;
+			}
+
+			//checks if user already tried dat number.
+			//if not, push to guesses array.
+
+			if (guesses.indexOf(intNumber) > -1) {
+				$("#feedback").html("You already tried that one...");
+				console.log("You already tried that one...");
+				return 0;//cya, folks!
+			} else {
+				guesses.push(intNumber);
+			}
+
+			//do some validation wheter the number is between 1 and 100.
+			if(number <= 0 || intNumber > 100) {
+				$("#feedback").html("Number must be between 1 and 100!");
+				console.log("Number must be between 1 and 100!");
+				return 0; //must... get... out... function.......
+			}
+
+			//Checks if win.
+			if ( theNumber === parseInt(intNumber)) {
+				$("#feedback").html("YOU WON!!!");
+				console.log("You won!!")
+				$("#submitGuess").prop("disabled",true);
+				$("#hint").prop("disabled",true);
+				return 0; //bye function.
+			}
+
+			//if didnt win, help the player.
+			else {
+				//says if its hot or cold.
+				if (Math.abs(theNumber - intNumber) < 15) {
+					feedback = "Hot!";
+					console.log("Hot!");
+				}
+				else {
+					feedback = "Cold!";
+					console.log("Cold!");
+				}
+				//says if it should be smaller or higher.
+				if (theNumber - intNumber < 0) {
+					$("#feedback").html(feedback + " Try a smaller number...");
+					console.log("try a smaller number...");
+					}
+				else {
+					$("#feedback").html(feedback + " Try a higher number...");
+					console.log("try a higher number...")
+				}
+				//nice try?
+				lifes--;
+				$("#remainingGuesses").html(lifes + " guesses remainining.");
+				if (lifes === 0) {
+					$("#feedback").html("GAME OVER, PAL!");
+				}
+			}
+		} else {
+			//no lifes? GAME OVER!!!
+			console.log("Game Over, man.");
+			return 0;
+		}
+	}
+
 	$("#remainingGuesses").html(lifes + " guesses remainining.");
 	//attach button click to function play with number on the input
 	$("#submitGuess").on("click",function(event) {
@@ -10,98 +109,3 @@ $(document).ready(function(){
 		resetGame();
 	});
 });
-
-var theNumber = generateRandom(); //random number
-var lifes = 5; //Amount of permitted guesses before GAME OVER
-var guesses = []; // What the user tried so far.
-
-function generateRandom () {
-	//this function generates a random number between 1 & 100.
-	return Math.round(Math.random() * 100 + 1);
-}
-
-function resetGame(){
-	//self explanatory
-	theNumber = generateRandom();
-	lifes = 5;
-	guesses	= [];
-}
-
-function play(number){
-	//main play function :D yayy
-	var intNumber = parseInt(number);
-	var feedback = "";
-	//if no lifes, GAME OVER, son.
-	if (lifes > 0) {
-		//do a small validation wheter it is a number.
-		if(isNaN(intNumber)){
-			$("#feedback").html("Must choose a number between 1 and 100!");
-			console.log("Must choose a number between 1 and 100!");
-			return 0;
-		}
-
-		//checks if user already tried dat number.
-		//if not, push to guesses array.
-
-		if (guesses.indexOf(intNumber) > -1) {
-			$("#feedback").html("You already tried that one...");
-			console.log("You already tried that one...");
-			return 0;//cya, folks!
-		} else {
-			guesses.push(intNumber);
-		}
-
-		//do some validation wheter the number is between 1 and 100.
-		if(number <= 0 || intNumber > 100) {
-			$("#feedback").html("Number must be between 1 and 100!");
-			console.log("Number must be between 1 and 100!");
-			return 0; //must... get... out... function.......
-		}
-
-		//Checks if win.
-		if ( theNumber === parseInt(intNumber)) {
-			$("#feedback").html("YOU WON!!!");
-			console.log("You won!!")
-			$("#submitGuess").prop("disabled",true);
-			$("#hint").prop("disabled",true);
-			return 0; //bye function.
-		}
-
-
-		//if didnt win, help the player.
-		else {
-
-			//says if its hot or cold.
-			if (Math.abs(theNumber - intNumber) < 15) {
-				feedback = "Hot!";
-				console.log("Hot!");
-			}
-			else {
-				feedback = "Cold!";
-				console.log("Cold!");
-			}
-
-			//says if it should be smaller or higher.
-			if (theNumber - intNumber < 0) {
-				$("#feedback").html(feedback + " Try a smaller number...");
-				console.log("try a smaller number...");
-				}
-			else {
-				$("#feedback").html(feedback + " Try a higher number...");
-				console.log("try a higher number...")
-			}
-
-			//nice try?
-			lifes--;
-			$("#remainingGuesses").html(lifes + " guesses remainining.");
-			if (lifes === 0) {
-				$("#feedback").html("GAME OVER, PAL!");
-
-			}
-		}
-	} else {
-		//no lifes? GAME OVER!!!
-		console.log("Game Over, man.");
-		return 0;
-	}
-}
