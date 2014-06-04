0e86e4b56e5c452fee54b922e86e4ac861123a0c
Cesar Vargas <cesarvargas@Cesars-MacBook-Pro.local>
2014-06-04 00:13:01 -0400

    did all exercises for today.

---
+++ part3/test_first_javascript/05_pig_latin/pigLatin.js
@@ -0,0 +1,19 @@
+function translate(str) {
+
+	function isVowel(c) {
+		var vowels = ["a","e","i","o","u"];
+		for (var i = 0; i < vowels.length; i++)
+			if(c === vowels[i]) return true;
+		return false;
+	}
+
+	var ar = str.split(" "); //creates an array
+	for(var i = 0; i < ar.length; i++){
+		while(!isVowel(ar[i].charAt(0)))
+			ar[i] = ar[i].slice(1) + ar[i].charAt(0);
+			if(ar[i].charAt(0) === "u" && ar[i].slice(-1) === "q")
+				ar[i] = ar[i].slice(1) + ar[i].charAt(0);
+		ar[i] += "ay";
+	}
+	return ar.join(" ");
+}
